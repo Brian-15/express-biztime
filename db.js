0385c2fd92e4b0e66b2b314d9e2f59c3fa412396
Brian-15 <bcirieco@gmail.com>
2021-10-25 17:48:16 -0700

    connect db and export object

--- db.js
+++ db.js
@@ -1,2 +1,11 @@
 /** Database setup for BizTime. */
 
+const { Client } = require('pg');
+
+const DB_URI = 'postgresql:///biztime';
+
+let db = new Client({ connectionString: DB_URI });
+
+db.connect();
+
+module.exports = db;
